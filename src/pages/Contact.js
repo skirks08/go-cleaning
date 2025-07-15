110588706762fe8d00e6c2ef74b691ba634bafba
Michael Skirka <skirkamp@hotmail.com>
2025-07-15 21:53:15 +0000

    contact page base content and styling

---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Contact.css';
+
+function Contact() {
+    return (
+        <div className="contact-page">
+            <h1>Contact Us</h1>
+            <p>We'd love to hear from you! Reach us using the info or form below.</p>
+            {/* Contact form goes here */}
+        </div>
+    );
+}
+
+export default Contact;
