557ad4dc09a50d47e6af744ccef6950717fc5c12
Michael Skirka <skirkamp@hotmail.com>
2025-07-15 21:50:38 +0000

    booking section base content

---
+++ src/pages/Booking.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Booking.css';
+
+function Booking() {
+    return (
+        <div className="booking-page">
+            <h1>Book a Cleaning!</h1>
+            <p>Select your preferred date, time, and service package to book an appointment.</p>
+            {/*Booking form and calendar goes here*/}
+        </div>
+    );
+}
+
+export default Booking;
